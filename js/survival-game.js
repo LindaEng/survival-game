033a73408b842f497ba09d0ad54dc9a0a223542c
= <90lineng@gmail.com>
2022-12-09 15:16:03 -0500

    added mainscene

--- js/survival-game.js
+++ js/survival-game.js
@@ -1,24 +1,23 @@
+import MainScene from "./MainScene.js";
 const config = {
-    width: 512,
+    width: 512, 
     height: 512,
-    backgroundColor: '#333333',
+    backgroundColor: "#333333",
     type: Phaser.AUTO,
-    parent: 'survival-game',
-    scene: [],
+    parent: "survival-game",
+    scene: [MainScene],
     scale: {
         zoom: 2,
     },
     physics: {
-        default: 'matter',
+        default: "matter",
         matter: {
             debug: true,
-            gravity: {
-                y:0
-            }
+            gravity: {y:0},
         }
     },
     plugins: {
-        scene: [
+        scene:[
             {
                 plugin: PhaserMatterCollisionPlugin.default,
                 key: 'matterCollision',
@@ -28,4 +27,4 @@ const config = {
     }
 }
 
-new Phaser.Game(config)
+new Phaser.Game(config);
