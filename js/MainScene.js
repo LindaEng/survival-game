8a94058b1e5428456c52cc0a94f6521aedf52fea
= <90lineng@gmail.com>
2022-12-09 20:25:04 -0500

    Player collider and sensor added - also removed rotation

--- js/MainScene.js
+++ js/MainScene.js
@@ -12,6 +12,7 @@ export default class MainScene extends Phaser.Scene {
     create() {
         console.log("create")
         this.player = new Player({scene: this.matter.world, x: 0, y: 0, texture: 'female', frame: 'townsfolk_f_idle_1'});
+        let test = new Player({scene: this.matter.world, x: 100, y: 100, texture: 'female', frame: 'townsfolk_f_idle_1'});
         this.player.inputKeys = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
--- js/Player.js
+++ js/Player.js
@@ -3,11 +3,25 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         let {scene, x, y, texture, frame} = data
         super(scene, x, y, texture, frame)
         this.scene.add.existing(this)
+
+        const {Body, Bodies} = Phaser.Physics.Matter.Matter
+        let playerCollider = Bodies.circle(this.x, this.y, 12, { isSensor: false, label: 'playerCollider'})
+        let playerSensor = Bodies.circle(this.x, this.y, 24, { isSensor: true, label: 'playerSensor'})
+        const compoundBody = Body.create({
+            parts: [playerCollider, playerSensor],
+            frictionAir: 0.35
+        })
+        this.setExistingBody(compoundBody)
+        this.setFixedRotation()
     }
 
     static preload(scene) {
         scene.load.atlas('female', 'assets/images/female.png', 'assets/images/female_atlas.json')
         scene.load.animation('female_anim', 'assets/images/female_anim.json')
+    }
+
+    get velocity() {
+        return this.body.velocity
     }
 
     update() {
@@ -26,5 +40,11 @@ export default class Player extends Phaser.Physics.Matter.Sprite {
         playerVelocity.normalize()
         playerVelocity.scale(speed);
         this.setVelocity(playerVelocity.x, playerVelocity.y);
+
+        if(Math.abs(this.velocity.x) > 0.1 || Math.abs(this.velocity.y) > 0.1) {
+            this.anims.play('female_walk', true)
+        } else {
+            this.anims.play('female_idle', true)
+        }
     }
 }
