8980ca5c4fe7073398422422a684c673745dc86f
= <90lineng@gmail.com>
2022-12-11 21:44:38 -0500

    added two map layers

--- js/MainScene.js
+++ js/MainScene.js
@@ -7,12 +7,16 @@ export default class MainScene extends Phaser.Scene {
 
     preload() {
         Player.preload(this)
+        this.load.image('tiles', 'assets/images/RPG Nature Tileset.png')
+        this.load.tilemapTiledJSON('map', 'assets/images/map.json')
     }
 
     create() {
-        console.log("create")
+        const map = this.make.tilemap({key: 'map'})
+        const tileset = map.addTilesetImage('RPG Nature Tileset', 'tiles', 32, 32, 0, 0)
+        const layer1 = map.createStaticLayer('Tile Layer 1', tileset, 0, 0)
+        const layer2 = map.createStaticLayer('Tile Layer 2', tileset, 0, 0)
         this.player = new Player({scene: this.matter.world, x: 0, y: 0, texture: 'female', frame: 'townsfolk_f_idle_1'});
-        let test = new Player({scene: this.matter.world, x: 100, y: 100, texture: 'female', frame: 'townsfolk_f_idle_1'});
         this.player.inputKeys = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
             down: Phaser.Input.Keyboard.KeyCodes.S,
