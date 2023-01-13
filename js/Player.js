9cbfd1f04a1adbda458a54daa0ffff15bd21cec4
= <90lineng@gmail.com>
2023-01-13 18:14:51 -0500

    Matter entity class refactored

--- js/Player.js
+++ js/Player.js
@@ -1,132 +1,135 @@
-    export default class Player extends Phaser.Physics.Matter.Sprite {
-    constructor(data) {
-        let {scene, x, y, texture, frame} = data
-        super(scene, x, y, texture, frame)
-        this.touching = []
-        this.scene.add.existing(this)
-        //weapon - pickaxe is at cell 162
-        this.spriteWeapon = new Phaser.GameObjects.Sprite(this.scene, 0, 0, 'items', 238)
-        //scale the size of weapon down
-        this.spriteWeapon.setScale(0.8)
-        //set the origin
-        this.spriteWeapon.setOrigin(0.05, 0.75)
-        this.scene.add.existing(this.spriteWeapon)
+import MatterEntity from "./MatterEntity.js";
 
-        const {Body, Bodies} = Phaser.Physics.Matter.Matter
-        let playerCollider = Bodies.circle(this.x, this.y, 12, { isSensor: false, label: 'playerCollider'})
-        let playerSensor = Bodies.circle(this.x, this.y, 24, { isSensor: true, label: 'playerSensor'})
-        const compoundBody = Body.create({
-            parts: [playerCollider, playerSensor],
-            frictionAir: 0.35
-        })
-        this.setExistingBody(compoundBody)
-        this.setFixedRotation()
-        //method to handle when player collides with game object
-        this.CreateMiningCollisions(playerSensor)
-        this.CreatePickupCollisions(playerCollider)
-        //flip our character if our pointer is facing left
-        this.scene.input.on('pointermove', pointer => this.setFlipX(pointer.worldX < this.x))
+export default class Player extends MatterEntity {
+  constructor(data){
+    let {scene,x,y,texture,frame} = data;
+    console.log('PLAYER SCENE ', scene)
+    super({...data,health:2,drops:[],name:'player'});
+    this.touching = [];
+    //Weapon
+    this.spriteWeapon = new Phaser.GameObjects.Sprite(this.scene,0,0,'items',162);
+    this.spriteWeapon.setScale(0.8);
+    this.spriteWeapon.setOrigin(0.25,0.75);
+    this.scene.add.existing(this.spriteWeapon);
+
+    const {Body,Bodies} = Phaser.Physics.Matter.Matter;
+    var playerCollider = Bodies.circle(this.x,this.y,12,{isSensor:false,label:'playerCollider'});
+    var playerSensor = Bodies.circle(this.x,this.y,24, {isSensor:true, label:'playerSensor'});
+    const compoundBody = Body.create({
+      parts:[playerCollider,playerSensor],
+      frictionAir: 0.35,
+    });
+    this.setExistingBody(compoundBody);
+    this.setFixedRotation();
+    this.CreateMiningCollisions(playerSensor);
+    this.CreatePickupCollisions(playerCollider);
+    this.scene.input.on('pointermove',pointer => { if(!this.dead) this.setFlipX(pointer.worldX < this.x)});
+  }
+
+  static preload(scene){
+    scene.load.atlas('female','assets/images/female.png','assets/images/female_atlas.json');
+    scene.load.animation('female_anim','assets/images/female_anim.json');
+    scene.load.spritesheet('items','assets/images/items.png',{frameWidth:32,frameHeight:32});
+    scene.load.audio('player','assets/audio/player.mp3');
+  }
+
+  onDeath = () => {
+    this.anims.stop();
+    this.setTexture('items',0);
+    this.setOrigin(0.5);
+    this.spriteWeapon.destroy();
+  }
+
+  update(){
+    if(this.dead) return;
+    const speed = 2.5;
+    let playerVelocity = new Phaser.Math.Vector2();
+    if(this.inputKeys.left.isDown) {
+      playerVelocity.x = -1;
+    } else if (this.inputKeys.right.isDown) {
+      playerVelocity.x = 1;
+    }
+    if(this.inputKeys.up.isDown) {
+      playerVelocity.y = -1;
+    } else if (this.inputKeys.down.isDown) {
+      playerVelocity.y = 1;
+    }
+    playerVelocity.normalize();
+    playerVelocity.scale(speed);
+    this.setVelocity(playerVelocity.x,playerVelocity.y);
+    if(Math.abs(this.velocity.x) > 0.1 || Math.abs(this.velocity.y) > 0.1) {
+      this.anims.play('female_walk',true);
+    }else {
+      this.anims.play('female_idle',true);
+    }
+    this.spriteWeapon.setPosition(this.x,this.y);
+    this.weaponRotate();
+  }
+
+  weaponRotate(){
+    let pointer = this.scene.input.activePointer;
+    if(pointer.isDown){
+      this.weaponRotation += 6;
+    }else{
+      this.weaponRotation = 0;
+    }
+    if(this.weaponRotation > 100) {
+      this.whackStuff();
+      this.weaponRotation = 0;
     }
 
-    static preload(scene) {
-        scene.load.atlas('female', 'assets/images/female.png', 'assets/images/female_atlas.json')
-        scene.load.animation('female_anim', 'assets/images/female_anim.json')
-        scene.load.spritesheet('items', 'assets/images/items.png', {frameWidth:32, frameHeight:32})
+    if(this.flipX){
+      this.spriteWeapon.setAngle(-this.weaponRotation - 90);
+    }else{
+      this.spriteWeapon.setAngle(this.weaponRotation);
     }
+  }
 
-    get velocity() {
-        return this.body.velocity
-    }
+  CreateMiningCollisions(playerSensor){
+    this.scene.matterCollision.addOnCollideStart({
+      objectA:[playerSensor],
+      callback: other => {
+        if(other.bodyB.isSensor) return;
+        this.touching.push(other.gameObjectB);
+        console.log(this.touching.length,other.gameObjectB.name);
+      },
+      context: this.scene,
+    });
 
-    update() {
-        this.anims.play('female_walk', true)
-        const speed = 2.5;
-        let playerVelocity = new Phaser.Math.Vector2();
-        if(this.inputKeys.left.isDown){
-            playerVelocity.x = -1;
-        }else if(this.inputKeys.right.isDown){
-            playerVelocity.x = 1;
-        }else if(this.inputKeys.up.isDown){
-            playerVelocity.y = -1;
-        }else if(this.inputKeys.down.isDown){
-            playerVelocity.y = 1;
-        }
-        playerVelocity.normalize()
-        playerVelocity.scale(speed);
-        this.setVelocity(playerVelocity.x, playerVelocity.y);
+    this.scene.matterCollision.addOnCollideEnd({
+      objectA:[playerSensor],
+      callback: other => {
+        this.touching = this.touching.filter(gameObject => gameObject != other.gameObjectB);
+        console.log(this.touching.length);
+      },
+      context: this.scene,
+    })
+  }
 
-        if(Math.abs(this.velocity.x) > 0.1 || Math.abs(this.velocity.y) > 0.1) {
-            this.anims.play('female_walk', true)
-        } else {
-            this.anims.play('female_idle', true)
-        }
-        this.spriteWeapon.setPosition(this.x, this.y)
-        this.weaponRotate()
-    }
+  CreatePickupCollisions(playerCollider){
+    this.scene.matterCollision.addOnCollideStart({
+      objectA:[playerCollider],
+      callback: other => {
+        if(other.gameObjectB && other.gameObjectB.pickup) other.gameObjectB.pickup();
+      },
+      context: this.scene,
+    });
 
-    weaponRotate() {
-        //mouse click
-        let pointer = this.scene.input.activePointer
-        if(pointer.isDown) {
-            this.weaponRotation += 6
-        } else {
-            this.weaponRotation = 0
-        }
-        if(this.weaponRotation > 100) {
-            this.wackStuff()
-            this.weaponRotation = 0
-        }
-        if(this.flipX) {
-            //flip weapon to left
-            this.spriteWeapon.setAngle(-this.weaponRotation - 90)
-        } else {
-            //if we are facing right
-            this.spriteWeapon.setAngle(this.weaponRotation)
-        }
-    }
+    this.scene.matterCollision.addOnCollideActive({
+      objectA:[playerCollider],
+      callback: other => {
+        if(other.gameObjectB && other.gameObjectB.pickup) other.gameObjectB.pickup();
+      },
+      context: this.scene,
+    })
 
-    CreateMiningCollisions(playerSensor) {
-        //when we move towards a game object add to array
-        this.scene.matterCollision.addOnCollideStart({
-            objectA: [playerSensor],
-            callback: other => {
-                if(other.bodyB.isSensor) return
-                this.touching.push(other.gameObjectB)
-                console.log(this.touching.length, other.gameObjectB.name)
-            },
-            context: this.scene,
-        })
-        //when we move away from a game object -> remove from array
-        this.scene.matterCollision.addOnCollideEnd( {
-            objectA: [playerSensor],
-            callback: other => {
-                this.touching = this.touching.filter(gameObject => gameObject !== other.gameObjectB)
-            }
-        })
-    }
+  }
 
-    CreatePickupCollisions(playerCollider) {
-        //when we move towards a game object add to array
-        this.scene.matterCollision.addOnCollideStart({
-            objectA: [playerCollider],
-            callback: other => {
-                if(other.gameObjectB && other.gameObjectB.pickup) other.gameObjectB.pickup()
-            },
-            context: this.scene,
-        })
-        //when we move away from a game object -> remove from array
-        this.scene.matterCollision.addOnCollideActive( {
-            objectA: [playerCollider],
-            callback: other => {
-                if(other.gameObjectB && other.gameObjectB.pickup) other.gameObjectB.pickup() 
-            }
-        })
-    }
-    wackStuff() {
-        this.touching = this.touching.filter(gameObject => gameObject.hit && !gameObject.dead)
-        this.touching.forEach(gameObject => {
-            gameObject.hit()
-            if(gameObject.dead) gameObject.destroy()
-        })
-    }
+  whackStuff(){
+    this.touching = this.touching.filter(gameObject => gameObject.hit && !gameObject.dead);
+    this.touching.forEach(gameobject =>{
+      gameobject.hit();
+      if(gameobject.dead) gameobject.destroy();
+    })
+  }
 }
--- js/Resource.js
+++ js/Resource.js
@@ -1,6 +1,6 @@
-import DropItem from './DropItem.js'
+import MatterEntity from "./MatterEntity.js"
 
-export default class Resource extends Phaser.Physics.Matter.Sprite {
+export default class Resource extends MatterEntity{
     static preload(scene) {
         scene.load.atlas('resources', 'assets/images/resources.png', 'assets/images/resources_atlas.json')
         scene.load.audio('tree', 'assets/audio/tree.mp3')
@@ -8,42 +8,26 @@ export default class Resource extends Phaser.Physics.Matter.Sprite {
         scene.load.audio('bush', 'assets/audio/bush.mp3')
         scene.load.audio('pickup', 'assets/audio/pickup.mp3')
     }
+
     constructor(data) {
         let {scene, resource} = data
-        super(scene.matter.world, resource.x, resource.y, 'resources', resource.properties.find(item => item.name === 'type').value)
-        this.scene.add.existing(this)
-        //adding health to item
-        this.health = 5
-        this.drops = JSON.parse(resource.properties.find(item => item.name == 'drops').value)
-        //required when working with collider
-        this.name = resource.properties.find(item => item.name === 'type').value
-        //assigning audio to sound
-        this.sound = this.scene.sound.add(this.name)
-        let yOrigin = resource.properties.find(item => item.name === 'yOrigin').value
+        let drops = JSON.parse(resource.properties.find(item => item.name == 'drops').value)
+        let depth = resource.properties.find(item => item.name == 'depth').value
+        let name = resource.properties.find(item => item.name === 'type').value 
 
-        this.x += this.width/2
-        this.y -= this.height/2
+        super({scene, x: resource.x, y: resource.y, texture: 'resources', frame: name, drops, depth, health: 5, name})
+
+        this.scene.add.existing(this)
+
+        let yOrigin = resource.properties.find(item => item.name === 'yOrigin').value
         this.y = this.y + this.height * (yOrigin - 0.9)
 
-        const {Body,Bodies} = Phaser.Physics.Matter.Matter
+        const {Bodies} = Phaser.Physics.Matter.Matter
         let circleCollider = Bodies.circle(this.x, this.y, 12, {isSensor: false, label:'collider'})
-
         this.setExistingBody(circleCollider)
+        
         this.setStatic(true)
         this.setOrigin(0.5)
     }
 
-    get dead() {
-        return this.health <= 0
-    }
-
-    hit = () => {
-        if(this.sound) this.sound.play()
-        this.health--
-        console.log(`Hitting ${this.nae} Health: ${this.health}`)
-        if(this.dead) {
-            console.log('DROPS ', this.drops)
-            this.drops.forEach(drop => new DropItem({scene: this.scene, x: this.x, y: this.y, frame: drop}))
-        }
-    }
 }
